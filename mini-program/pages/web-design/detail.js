1c5fafd8962d91ee3b1ce6c33d51d50478d2824d
XiaoSaurus <XiaoSaurus@aliyun.com>
2026-04-12 07:22:29 +0800

    feat: 网页设计详情页支持公众号文章web-view嵌入

--- mini-program/pages/web-design/detail.js
+++ mini-program/pages/web-design/detail.js
@@ -1,12 +1,18 @@
 const { request } = require('../../utils/request.js')
 
 Page({
-  data: { detail: null },
+  data: {
+    detail: null,
+    articleUrl: ''
+  },
 
   onLoad(options) {
     if (options.id) {
       request(`/web-design/${options.id}`).then(res => {
-        this.setData({ detail: res.data })
+        this.setData({
+          detail: res.data,
+          articleUrl: res.data.articleUrl || ''
+        })
       })
     }
   }
